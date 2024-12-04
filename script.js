85a5d5696d611d7e6e087d785c230007c925ccc0
Caesar <juliofolee@gmail.com>
2024-12-03 21:06:47 -0400

    changed readme

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 function toggleMode() {
   const html = document.documentElement
-
-  // if(html.classList.contains('light')) {
-  //    html.classList.remove('light')
-  //  } else {
-  //    html.classList.add('light')
-  //  }
-
   html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute(
+      "src",
+      "./assets/assets/avatar-light.png",
+      img.setAttribute(
+        "alt",
+        "Foto de Julio Cezar com uma face séria com uma camisa azul em um fundo amadeirado"
+      )
+    )
+  } else {
+    img.setAttribute(
+      "src",
+      "./assets/assets/avatar.png",
+      img.setAttribute(
+        "alt",
+        "Foto de Julio Cezar de oculos, com uma face séria e uma camisa social branca com um fundo amadeirado"
+      )
+    )
+  }
 }
